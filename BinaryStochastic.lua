43f6b150f296e0c926be8b1ebcf2c6cbfd664fda
Binesh Bannerjee <binesh_binesh@hotmail.com>
2016-07-29 21:39:08 -0400

    Changed BinaryStochastic to yield arbitrary values

--- BinaryStochastic.lua
+++ BinaryStochastic.lua
@@ -1,8 +1,10 @@
 local BinaryStochastic = torch.class('nn.BinaryStochastic', 'nn.Sigmoid')
 
-function BinaryStochastic:__init()
+function BinaryStochastic:__init(lo, hi)
 	nn.Sigmoid.__init(self)
 	self.rawoutput = torch.Tensor()
+	self.lo = lo or 0
+	self.hi = hi or 1
 end
 
 function BinaryStochastic:updateOutput(input)
@@ -32,7 +34,10 @@ function BinaryStochastic:updateOutput(input)
 -- Why the scale from 0..1 to 0.1..1.9? Because, if we simply multiplied by 2
 -- we _could_ get 0..2 (_if_ the input was _actually_ 1 (finite numeric precision and all that)
 -- So, instead, we scale 0..1 to 0.1..1.9 and just floor that.
-		self.output:mul(1.8):add(0.1):floor()
+--      b == lo
+-- a      == (hi-lo)
+
+		self.output:mul(1.8):add(0.1):floor():mul(self.hi-self.lo):add(self.lo)
 	end
 	return self.output
 end
